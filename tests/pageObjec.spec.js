ab9791068e4356fcadbd3d9869f9fb19d87c3c1e
deepak-game <deepakkumarjha233@gmail.com>
2025-12-03 11:40:06 +0530

    updated all files

--- tests/pageObjec.spec.js
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-import Login from "./login";
-
-test("Do Login", async ({ page }) => {
-  const login = new Login(page);
-  await login.goto();
-  await login.performLogin();
-  await page.waitForTimeout(5.0);
-});
